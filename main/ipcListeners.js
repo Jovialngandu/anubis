5f1c61fb5b7fbba60530e33e8fb06cd70e52d626
Jovialngandu <jovialngandu2@gmail.com>
2024-11-29 22:41:22 +0100

    [ADD|UPDATE] Ajout des handlers(ecouteurs) necessaires

--- main/ipcListeners.js
+++ main/ipcListeners.js
@@ -1,14 +1,40 @@
-//gestion des ecouteurs ipc
+//gestion des ecouteurs ipc 'findWhere','findFirst','findLast','delete','deleteWhere' ,'findAll','findbyId'
 const { ipcMain } = require('electron');
+const Model = require('../database/model');
+const toHandleWithArgs=['findWhere','findById','insert'];//findWhere,delete,Update,deleteWhere
+const toHandleWithouthArgs=['findAll','findFirst','findLast'];//findFirt,findLast,
+const toOn=['createTable']
+const tableName=['Etiquette_Task','Etiquette','List','Note','Project','Task']
 
-function setupIpcListeners(mainWin) {//on pourait avoir besoin d'effectuer des operztion sur la fenetre 
- 
-  ipcMain.handle('ping', ()=>'pong');
-  ipcMain.on('yo', (event, args) => {
-    console.log('event',event)
-    console.log('args',args)
-  })
+async function setupIpcListeners(mainWin) {//on pourait avoir besoin d'effectuer des operztion sur la fenetre 
+
+  tableName.forEach(tableName => {
+    let tableLink= require('../app/model/'+tableName)
+
+    toHandleWithArgs.forEach( handle=> {
+      ipcMain.handle(tableName+':'+handle,async (event,args=[])=>{ 
+        const datas=await tableLink[handle](...args) 
+        return datas
+
+      });
+
+    });
+
+    toHandleWithouthArgs.forEach( handle=> {
+      ipcMain.handle(tableName+':'+handle,async (event)=>{
+        
+        const datas=await tableLink[handle]()
+        console.log(datas)
+        return datas
+
+      });
+
+    });
+    
+  });
+
   console.log('IpcListener Done.');
+ 
 }
 
 module.exports = setupIpcListeners;
