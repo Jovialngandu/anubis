8b75d900798131e01769fbaeb2b6cc5b50e6a6b3
Jovialngandu <jovialngandu2@gmail.com>
2024-11-29 12:30:41 +0100

    [ADD] Creation du model List

---
+++ app/model/list.js
@@ -0,0 +1,16 @@
+Model=require('../../database/model')
+
+class List extends Model {
+    constructor() {
+      super('note', {
+        id: 'INTEGER PRIMARY KEY AUTOINCREMENT',
+        name: 'TEXT NOT NULL',
+        describtion: 'TEXTE ',
+        project_id:' INTEGER'
+      },
+      ' ,FOREIGN KEY (project_id) REFERENCES project(id) ON DELETE CASCADE ON UPDATE CASCADE'//ne pas oublier la virgule avant
+    );
+    }
+  }
+  
+  module.exports = new List();
